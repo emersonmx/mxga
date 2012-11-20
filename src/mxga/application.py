45719e8deb036652bb5509f96b2225aebc78fbcf
Emerson MX <emersonmx@gmail.com>
2012-11-19 21:29:06 -0300

    Basic genetic framework.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

---
+++ src/mxga/application.py
@@ -0,0 +1,44 @@
+class GenericApplication(object):
+'''Default genetic application.
+'''
+
+    def __init__(self):
+        object.__init__(self)
+
+    def _initialize(self):
+        pass
+
+    def _finalize(self):
+        pass
+
+    def _best_fitness(self):
+        '''Returns the best fitness.
+        '''
+        pass
+
+    def _select(self):
+        pass
+
+    def _crossover(self, chromosomes):
+        pass
+
+    def _mutation(self, chromosomes):
+        pass
+
+    def _update_next_population(self, chromosomes):
+        pass
+
+    def run(self, delta=0.01, epoch_number=1000000):
+        epoch = 1
+        self._initialize()
+
+        while self._best_fitness() > delta and epoch <= epoch_number:
+            parental_chromosomes = self._select()
+
+            if chromosomes:
+                mutated_chromosomes = self._mutation(chromosomes)
+
+                self._update_next_population(mutated_chromosomes)
+
+            epoch += 1
+
---
+++ src/mxga/crossover.py
@@ -0,0 +1,39 @@
+def split(parental_chromosomes, pivot):
+'''Create a new chromosome using 2 parental_chromosomes,
+don't bothering the gene order.
+
+Parameters:
+    parental_chromosomes: two gene list [[g1, g2, g3, ..., gn],
+                          [g1, g2, g3, ..., gn]]
+'''
+    p1, p2 = parental_chromosomes
+
+    gene_number = len(p1)
+    return p1[:pivot] + p2[pivot:gene_number]
+
+def cycle(parental_chromosomes):
+'''Create a new chromosome using 2 parentes, keeping gene unicity.
+
+Parameters:
+    parental_chromosomes: two gene list [[g1, g2, g3, ..., gn],
+                          [g1, g2, g3, ..., gn]]
+'''
+    p1, p2 = parental_chromosomes
+    gene_number = len(p1)
+    chromosome = [None for gene in range(gene_number)]
+
+    i = 0
+    ciclic = False
+    while not ciclic:
+        chromosome[i] = p1[i]
+        gene = p2[i]
+        i = p1.index(gene)
+
+        if p1[i] in chromosome:
+            ciclic = True
+
+    for i in range(gene_number):
+        if not chromosome[i]:
+            chromosome[i] = p2[i]
+
+    return chromosome
