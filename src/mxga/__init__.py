0fa8a50d7a5e773f193be95840be200e3b654502
Emerson MX <emersonmx@gmail.com>
2012-11-19 22:01:24 -0300

    Basic chromosome added.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

---
+++ src/mxga/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['application', 'chromosome', 'crossover', 'mutation']
---
+++ src/mxga/chromosome.py
@@ -0,0 +1,7 @@
+class Chromosome(object):
+
+    def __init__(self):
+        object.__init__(self)
+
+        self.genes = []
+        self.fitness = 0
